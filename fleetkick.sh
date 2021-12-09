98755060597234356f29b0529163fb089764a77d
Jonathan Holloway <jholloway@redhat.com>
2021-12-08 20:45:06 -0600

    add configfile and tag file injection
    Signed-off-by: Jonathan Holloway <jholloway@redhat.com>

--- fleetkick.sh
+++ fleetkick.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 : <<'__usage__'
 
-KS_FQPATH: Fully qualified path to kickstart file
-ISO_IN: Path to source ISO
-ISO_OUT: Path to target ISO
+KICKSTART: Fully qualified path to kickstart file
+SOURCE_ISO: Path to source ISO
+OUTPUT_ISO: Path to target ISO
 WORKDIR: Path to the directory the source ISO will be exploded into.
-REG_FQPATH: Fully qualified path to registration credential file
+RHC_VARS: Fully qualified path to registration credential file
 
 ./fleetkick.sh -k kickstart-stage.ks \
--i /var/lib/libvirt/images/composer-api-d83040bf-b410-4317-9121-1ee7bd726ac1-rhel84-boot.iso \
--o /var/lib/libvirt/images/ksinjected/composer-api-d83040bf-b410-4317-9121-1ee7bd726ac1-rhel84-boot_notefi.iso \
--w /home/root/edgeiso/download98a \
+-i composer-api-d83040bf-b410-4317-9121-1ee7bd726ac1-rhel84-boot.iso \
+-o composer-api-d83040bf-b410-4317-9121-1ee7bd726ac1-rhel84-boot_notefi.iso \
 -r fleet_env.bash \
--p isodir/fleet_kspost.txt \
--s isodir/fleet_authkeys.txt
+-p fleet_kspost.txt \
+-s fleet_authkeys.txt \
+-t fleet_tags
 
 __usage__
 
@@ -66,6 +66,7 @@ explode_iso() {
 
     # Leaving this alternate 7z command here for reference...
     #[[ -e $ISO ]] && 7z x $ISO -o${DIR} || fatal_error 1 "No ISO file $ISO"
+    # xorriso allows us to explode the ISO instead of loopback mount inside a container
     [[ -e "$ISO" ]] && xorriso -osirrox on -indev "$ISO" -extract / $DIR
 }
 
@@ -101,10 +102,9 @@ edit_bootconfig() {
 
     [[ -e $CONFIG ]] && file $CONFIG || fatal_error 1 "Boot config $CONFIG file does not exist"
 
-    #sed -i "/rescue/n;/LABEL=${VOLID}/ s/$/ inst.ks=hd:LABEL=${VOLID}:\/${KICKFILE} None/" $CONFIG
-    # Replace an existing inst.ks instruction
+    # Remove an existing inst.ks instruction
     sed -i "/rescue/n;/LABEL=${VOLID}/ s/\<inst.ks[^ ]*//g" $CONFIG
-    # Replace an existing inst.ks instruction
+    # Replace an existing inst.ks instruction (redundant)
     sed -i "/rescue/n;/LABEL=${VOLID}/ s/\<inst.ks[^ ]*/inst.ks=hd:LABEL=${VOLID}:\/${KICKFILE} None/g" $CONFIG
     # Inject an inst.ks instruction
     sed -i "/inst.ks=/n;/rescue/n;/LABEL=${VOLID}/ s/$/ inst.ks=hd:LABEL=${VOLID}:\/${KICKFILE} None/g" $CONFIG
@@ -124,6 +124,7 @@ modify_efiboot_image() {
 
 
 # DEPRECATED: replaced by the grub injection via msdos tools
+#             to avoid loopback inside a container
 regen_efi_image() {
     EFI_IN=$1
     EFI_OUT=$2
@@ -189,49 +190,76 @@ declare -r INJECT_KICKSTART=2
 declare -r INJECT_USER=4
 declare -r INJECT_KSPOST=8
 declare -r INJECT_AUTHKEYS=16
+declare -r INJECT_TAGS=32
 
 function main() {
 
+    # Set defaults
+    BASEDIR="/isodir"
+    WORKDIR="/tmp/working"
+    OUTPUT_ISO="fleet_out.iso"
+    [[ -e "${BASEDIR}/fleet.ks" ]] && KICKSTART="fleet.ks"
+    [[ -e "${BASEDIR}/fleet.kspost" ]] && KICKSTART_POST="fleet.kspost"
+    [[ -e "${BASEDIR}/fleet_source.iso" ]] && SOURCE_ISO="fleet_source.iso"
+    [[ -e "${BASEDIR}/fleet_rhc_vars" ]] && RHC_VARS="fleet_rhc_vars"
+    # TODO: consider reading admin user from # comment in fleet_authkeys file
+    [[ -e "${BASEDIR}/fleet_authkeys" ]] && AUTH_KEYS="fleet_authkeys" && ADMIN_USER="fleetmgmtadmin"
+    [[ -e "${BASEDIR}/fleet_tags" ]] && TAG_FILE="fleet_tags"
+    [[ -e "${BASEDIR}/fleet.conf" ]] && source "${BASEDIR}/fleet.conf"
+
+    # Override defaults with command-line options
     inject_files=0
-    while getopts 'k:i:o:w:r:p:u:s:' OPTION
+    while getopts 'c:k:i:o:w:r:p:u:s:t:' OPTION
     do
         case "$OPTION" in
+            c)
+                # Config file (overrides defaults)
+                # TODO: test this to make sure arg order does not prevent override of config file
+                CONFIG_FILE="$OPTARG"
+                source "${BASEDIR}/${CONFIG_FILE}"
+                cat "${BASEDIR}/${CONFIG_FILE}"
+                ;;
             k)
                 # Kicktstart file
-                KS_FQPATH="$OPTARG"
-                inject_files=$(( $inject_files | $INJECT_KICKSTART ))
+                KICKSTART="$OPTARG"
+                #inject_files=$(( $inject_files | $INJECT_KICKSTART ))
                 ;;
             i)
                 # Source ISO
-                ISO_IN="$OPTARG"
+                SOURCE_ISO="$OPTARG"
                 ;;
             o)
                 # Output ISO
-                ISO_OUT="$OPTARG"
+                OUTPUT_ISO="$OPTARG"
                 ;;
             w)
-                # The working directory for ISO explosion
+                # The working directory for ISO explosion (default: /tmp/workdir)
                 WORKDIR="$OPTARG"
                 ;;
             r)
                 # The registration credentials env file
-                REG_FQPATH="$OPTARG"
-                inject_files=$(( $inject_files | $INJECT_ENV_FILE ))
+                RHC_VARS="$OPTARG"
+                #inject_files=$(( $inject_files | $INJECT_ENV_FILE ))
                 ;;
             p)
                 # Kickstart post section file
-                KS_POST_FILE="$OPTARG"
-                inject_files=$(( $inject_files | $INJECT_KSPOST ))
+                KICKSTART_POST="$OPTARG"
+                #inject_files=$(( $inject_files | $INJECT_KSPOST ))
                 ;;
             u)
                 # Add an admin user
                 ADMIN_USER="$OPTARG"
-                inject_files=$(( $inject_files | $INJECT_USER ))
+                #inject_files=$(( $inject_files | $INJECT_USER ))
                 ;;
             s)
                 # Inject an authorized_keys file to user specified in -u
-                AUTHKEYS_FQPATH="$OPTARG"
-                inject_files=$(( $inject_files | $INJECT_AUTHKEYS ))
+                AUTH_KEYS="$OPTARG"
+                #inject_files=$(( $inject_files | $INJECT_AUTHKEYS ))
+                ;;
+            t)
+                # Inject an authorized_keys file to user specified in -u
+                TAG_FILE="$OPTARG"
+                #inject_files=$(( $inject_files | $INJECT_AUTHKEYS ))
                 ;;
             ?)
                 echo "script usage: $(basename \$0) [-k ksfile] [-i input ISO] [-o output ISO] [-w wordir path] [-r registration file]" >&2
@@ -241,61 +269,84 @@ function main() {
     done
     shift "$(($OPTIND -1))"
 
+
+    # Set options and add base dir to file paths
+    [[ "${KICKSTART+x}" ]] && inject_files=$(( $inject_files | $INJECT_KICKSTART )) \
+        && KICKSTART="$BASEDIR/$KICKSTART"
+    [[ "${KICKSTART_POST+x}" ]] && inject_files=$(( $inject_files | $INJECT_KSPOST )) \
+        && KICKSTART_POST="${BASEDIR}/${KICKSTART_POST}"
+    [[ "${RHC_VARS+x}" ]] && inject_files=$(( $inject_files | $INJECT_ENV_FILE )) \
+        && RHC_VARS="${BASEDIR}/${RHC_VARS}"
+    [[ "${ADMIN_USER+x}" ]] && inject_files=$(( $inject_files | $INJECT_USER ))
+    [[ "${AUTH_KEYS+x}" ]] && inject_files=$(( $inject_files | $INJECT_AUTHKEYS )) \
+        && AUTH_KEYS="${BASEDIR}/${AUTH_KEYS}"
+    [[ "${TAG_FILE+x}" ]] && inject_files=$(( $inject_files | $INJECT_TAGS)) \
+        && TAG_FILE="${BASEDIR}/${TAG_FILE}"
+    [[ "${SOURCE_ISO+x}" ]] && SOURCE_ISO="${BASEDIR}/${SOURCE_ISO}"
+    [[ "${OUTPUT_ISO+x}" ]] && OUTPUT_ISO="${BASEDIR}/${OUTPUT_ISO}"
+
+
     # Exit if kickstart and env files are not provided
-    if [[ -z ${KS_FQPATH+x} ]] && [[ -z ${REG_FQPATH+x} ]]
+    if [[ -z ${KICKSTART+x} ]] && [[ -z ${RHC_VARS+x} ]]
     then
         fatal_error 1 "No files to inject"
     fi
 
+    # Exit if a key file is provided without an admin user (it hoses logins)
+    [[ ! -n ${ADMIN_TEST_USER} ]] && [[ -n ${KEYFILE} ]] &&  fatal error 1 "Authorized keys file provided without admin user"
+
+    # Create the full workdir path
+    [[ ! -e "$WORKDIR" ]] && mkdir -p "$WORKDIR"
 
     # Open up the ISO
     if [ $inject_files -gt 0 ]
     then
-        [[ -e "$ISO_IN" ]] || fatal_error 1 "No source ISO file"
+        [[ -e "$SOURCE_ISO" ]] || fatal_error 1 "No source ISO file"
 
         # Get ISO Vol ID  (needed to identify the volume at install)
-        VOLID=$(get_iso_volid "$ISO_IN")
+        VOLID=$(get_iso_volid "$SOURCE_ISO")
         log_msg INFO "The ISO VOLID is $VOLID"
 
         # Copy the ISO internal contents to a working directory
-        explode_iso "$ISO_IN" $WORKDIR
+        explode_iso "$SOURCE_ISO" $WORKDIR
     fi
 
 
     # Insert the env file to the root of the working directory
-    (( $inject_files & $INJECT_ENV_FILE )) && insert_file "$REG_FQPATH" ${WORKDIR}/fleet_env.bash
+    (( $inject_files & $INJECT_ENV_FILE )) && insert_file "$RHC_VARS" ${WORKDIR}/fleet_env.bash
 
     # Insert the authkeys file to the root of the working directory
-    (( $inject_files & $INJECT_ENV_FILE )) && insert_file "$AUTHKEYS_FQPATH" ${WORKDIR}/fleet_authkeys.txt
+    (( $inject_files & $INJECT_AUTHKEYS )) && insert_file "$AUTH_KEYS" ${WORKDIR}/fleet_authkeys.txt
 
     # Inject the Kickstart file and configure installer boot
     if (( $inject_files & $INJECT_KICKSTART ))
     then
         # Validate the kickstart file
-        [[ -e "$KS_FQPATH" ]] || fatal_error 1 "No kickstart file"
-        #inject_custom_post /isodir/fleet_kspost.txt $"KS_FQPATH"
-        validate_kickstart "$KS_FQPATH" || fatal_error 1 "Kickstart file is not valid"
+        [[ -e "$KICKSTART" ]] || fatal_error 1 "No kickstart file"
+        #inject_custom_post /isodir/fleet_kspost.txt $"KICKSTART"
+        validate_kickstart "$KICKSTART" || fatal_error 1 "Kickstart file is not valid"
 
         ISOCFG="${WORKDIR}/isolinux/isolinux.cfg"
         EFICFG="${WORKDIR}/EFI/BOOT/grub.cfg"
         EFI_DIR="${WORKDIR}/EFI/BOOT"
         EFI_IMAGEPATH="${WORKDIR}/images/efiboot.img"
         KSFILE=fleet.ks
-        #KSFILE=`basename "$KS_FQPATH"`
-        #KSFILE=`basename ${KS_FQPATH// /_}`
+        #KSFILE=`basename "$KICKSTART"`
+        #KSFILE=`basename ${KICKSTART// /_}`
 
-        echo "KS_FQPATH: ${KS_FQPATH}"
-        echo "ISO IN: ${ISO_IN}"
-        echo "ISO OUT: ${ISO_OUT}"
+        echo "KICKSTART: ${KICKSTART}"
+        echo "ISO IN: ${SOURCE_ISO}"
+        echo "ISO OUT: ${OUTPUT_ISO}"
         echo "WORKDIR: ${WORKDIR}"
-        echo "REG_FQPATH: ${REG_FQPATH}"
+        echo "RHC_VARS: ${RHC_VARS}"
 
         echo "ISOCFG: ${ISOCFG}"
         echo "EFICFG: ${EFICFG}"
         echo "KSFILE: ${KSFILE}"
+        echo "TAGFILE: ${TAG_FILE}"
 
-        insert_file "$KS_FQPATH" ${WORKDIR}/${KSFILE}
-        #insert_file "$KS_FQPATH" ${WORKDIR}/finalKickstart-fleetmgmt.ks
+        insert_file "$KICKSTART" ${WORKDIR}/${KSFILE}
+        #insert_file "$KICKSTART" ${WORKDIR}/finalKickstart-fleetmgmt.ks
 
         # Edit boot files to point to the Kickstart file
         edit_bootconfig $ISOCFG $VOLID "$KSFILE"
@@ -309,23 +360,27 @@ function main() {
 
 
     # Inject a custom Kickstart post section from file
-    #(( $inject_files & $INJECT_KSPOST )) && inject_custom_post $KS_POST_FILE ${WORKDIR}/${KSFILE}
+    #(( $inject_files & $INJECT_KSPOST )) && inject_custom_post $KICKSTART_POST ${WORKDIR}/${KSFILE}
     #validate_kickstart ${WORKDIR}/${KSFILE} || fatal_error 1 "Kickstart file ${WORKDIR}/${KSFILE} is not valid"
-    (( $inject_files & $INJECT_KSPOST )) && insert_file "$KS_POST_FILE" ${WORKDIR}/fleet_kspost.txt
+    (( $inject_files & $INJECT_KSPOST )) && insert_file "$KICKSTART_POST" ${WORKDIR}/fleet_kspost.txt
+
+
+    # Inject an Insights tag file
+    (( $inject_files & $INJECT_TAGS )) && insert_file "$TAG_FILE" ${WORKDIR}/fleet_tags.yaml
 
 
     # Close things up
     if [ $inject_files -gt 0 ]
     then
         # Create a new bootable ISO
-        make_the_iso $WORKDIR "$ISO_OUT" $VOLID
+        make_the_iso $WORKDIR "$OUTPUT_ISO" $VOLID
         # Make it hybrid bootable
-        hybridify "$ISO_OUT"
+        hybridify "$OUTPUT_ISO"
         # Update the internal checksum
-        implant_md5 "$ISO_OUT"
+        implant_md5 "$OUTPUT_ISO"
 
         echo
-        log_msg INFO "New ISO $ISO_OUT created."
+        log_msg INFO "New ISO $OUTPUT_ISO created."
     fi
 }
 
@@ -339,8 +394,6 @@ fi
 # FIXME: Error checking logic for function calls
 # FIXME: Usage message
 # FIXME: Do we actually need None in the boot line?
-# TODO: add main logic
-# TODO: add default for ISO OUT
-# TODO: add config file (-c) instead of passing all arguments on cli
-# TODO: add debug mode
+# TODO: add debug mode and quiet this thing down by default
 # TODO: add script description to header (mention this does not require root)
+# FIXME: || fatal error might not be failure of the first argument to the &&, so fix those messages
